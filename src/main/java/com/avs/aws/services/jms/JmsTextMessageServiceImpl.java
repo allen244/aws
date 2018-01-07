a91bcdd0be8d35f8720832cd311d6bb8dcfe469c
asmaldone <alsmaldone@yahoo.com>
2018-01-07 16:21:23 -0500

    Spring dev ops

--- src/main/java/com/avs/aws/services/jms/JmsTextMessageServiceImpl.java
+++ src/main/java/com/avs/aws/services/jms/JmsTextMessageServiceImpl.java
@@ -1,6 +1,7 @@
 package com.avs.aws.services.jms;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.jms.core.JmsTemplate;
 import org.springframework.stereotype.Service;
 
@@ -12,13 +13,20 @@ import javax.jms.Queue;
 @Service
 public class JmsTextMessageServiceImpl implements JmsTextMessageService {
 
+
+
     private Queue textMessageQueue;
     private JmsTemplate jmsTemplate;
 
-    @Autowired
-    public void setTextMessageQueue(Queue textMessageQueue) {
-        this.textMessageQueue = textMessageQueue;
-    }
+//    public JmsTextMessageServiceImpl(Queue textMessageQueue, JmsTemplate jmsTemplate) {
+//        this.textMessageQueue = textMessageQueue;
+//        this.jmsTemplate = jmsTemplate;
+//    }
+
+//    @Autowired
+//    public void setTextMessageQueue(Queue textMessageQueue) {
+//        this.textMessageQueue = textMessageQueue;
+//    }
 
     @Autowired
     public void setJmsTemplate(JmsTemplate jmsTemplate) {
