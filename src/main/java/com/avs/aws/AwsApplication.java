14dd0f44e159ee19e640fda218146c0d7485cdd4
asmaldone <alsmaldone@yahoo.com>
2018-01-08 10:50:48 -0500

    Spring dev ops test

--- src/main/java/com/avs/aws/AwsApplication.java
+++ src/main/java/com/avs/aws/AwsApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AwsApplication {
 
+
+	//test
 	public static void main(String[] args) {
 		SpringApplication.run(AwsApplication.class, args);
 	}
